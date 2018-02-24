b01cfe93947f61d05433d37b67e9f652dfd1546b
Alex Cristea <alexcristea@me.com>
2018-02-24 16:20:37 +0200

    Refactoring 8: Use Move Method on getCharge method

--- VideoStore/Movie.swift
+++ VideoStore/Movie.swift
@@ -9,4 +9,27 @@ public class Movie {
         self.title = title
         self.priceCode = priceCode
     }
+
+    public func getCharge(daysRented:Int) -> Double {
+        var result:Double = 0
+
+        switch (priceCode) {
+        case .regular:
+            result += 2
+            if (daysRented > 2) {
+                result += Double(daysRented - 2) * 1.5
+            }
+
+        case .newRelease:
+            result += Double(daysRented) * 3
+
+        case .childern:
+            result += 1.5
+            if (daysRented > 3) {
+                result += Double(daysRented - 3) * 1.5
+            }
+        }
+
+        return result
+    }
 }
--- VideoStore/Rental.swift
+++ VideoStore/Rental.swift
@@ -11,26 +11,7 @@ public class Rental {
     }
 
     public func getCharge() -> Double {
-        var result:Double = 0
-
-        switch (movie.priceCode) {
-        case .regular:
-            result += 2
-            if (daysRented > 2) {
-                result += Double(daysRented - 2) * 1.5
-            }
-
-        case .newRelease:
-            result += Double(daysRented) * 3
-
-        case .childern:
-            result += 1.5
-            if (daysRented > 3) {
-                result += Double(daysRented - 3) * 1.5
-            }
-        }
-
-        return result
+        return movie.getCharge(daysRented: daysRented)
     }
 
     public func getFrequentRenterPoints() -> Int {
