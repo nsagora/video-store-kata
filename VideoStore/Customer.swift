89b0b599f65d228c727aa4f2abc008e109d09253
Alex Cristea <alexcristea@me.com>
2018-02-24 14:29:41 +0200

    Refactoring 2: Use Move Method on amount calculation

--- VideoStore/Customer.swift
+++ VideoStore/Customer.swift
@@ -21,7 +21,7 @@ public class Customer {
         
         for item in rentals {
             
-            let thisAmount = amount(forRental: item)
+            let thisAmount = item.getCharge()
             
             // add frequent renter points
             frequentRenterPoints += 1
@@ -38,29 +38,6 @@ public class Customer {
         result += "Amount owed is \(totalAmount)\n"
         result += "You earned \(frequentRenterPoints) frequent renter points\n"
         
-        return result
-    }
-
-    private func amount(forRental rental:Rental) -> Double {
-        var result:Double = 0
-
-        switch (rental.movie.priceCode) {
-        case .regular:
-            result += 2
-            if (rental.daysRented > 2) {
-                result += Double(rental.daysRented - 2) * 1.5
-            }
-
-        case .newRelease:
-            result += Double(rental.daysRented) * 3
-
-        case .childern:
-            result += 1.5
-            if (rental.daysRented > 3) {
-                result += Double(rental.daysRented - 3) * 1.5
-            }
-        }
-
         return result
     }
 }
