6edccae08ced9579192ce5bc83b29f4d9344a687
Alex Cristea <alexcristea@me.com>
2018-05-29 19:44:16 +0300

    Part 3
    Todo: Add JSON statement

--- VideoStore/Customer.swift
+++ VideoStore/Customer.swift
@@ -15,25 +15,41 @@ public class Customer {
     
     public func statement() -> String {
 
-        var frequentRenterPoints:Int = 0
         var result:String = "Rental Record for \(name)\n"
-        
         for rental in rentals {
-
-            frequentRenterPoints += rental.getFrequentRenterPoints()
-
             // show figures for this rental
             result += "\t\(rental.movie.title)\t\(rental.getCharge())\n"
         }
         
         // add footer lines
         result += "Amount owed is \(getTotalAmount())\n"
-        result += "You earned \(frequentRenterPoints) frequent renter points\n"
+        result += "You earned \(getFrequentRenterPoints()) frequent renter points\n"
         
+        return result
+    }
+
+    public func statementHTML() -> String {
+
+        var result:String = "<h1>Rental Record for \(name)</h1>\n"
+        result += "<ul>"
+        for rental in rentals {
+            // show figures for this rental
+            result += "<li>\(rental.movie.title)\t\(rental.getCharge())</li>"
+        }
+        result += "</ul>"
+
+        // add footer lines
+        result += "<p>Amount owed is \(getTotalAmount())</p>"
+        result += "<p>You earned \(getFrequentRenterPoints()) frequent renter points</p>"
+
         return result
     }
 
     private func getTotalAmount() -> Double {
         return rentals.reduce(0) { $0 + $1.getCharge()}
+    }
+
+    private func getFrequentRenterPoints() -> Int {
+        return rentals.reduce(0) { $0 + $1.getFrequentRenterPoints()}
     }
 }
